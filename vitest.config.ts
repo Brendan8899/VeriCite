e36e74871007bac56bca0e3236d1b2a6ab0e0180
JakeIsMeh <jakeismeh1@gmail.com>
2026-05-22 23:56:32 +0800

    oops 3

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,12 @@ export default defineConfig({
 		reporters: isCI ? ['default', 'github-actions'] : ['default'],
 		coverage: {
 			provider: 'v8',
-			// 'json-summary' is mandatory for the GitHub Action to read the data
+			enabled: true,
+			// Forces Vitest to write the coverage files even if a unit test fails
+			reportOnFailure: true,
+			// Explicitly forces the root directory destination
+			reportsDirectory: './coverage',
+			// 'json-summary' is for GitHub Action
 			reporter: isCI
 				? ['text', 'json-summary']
 				: ['text', 'html'],

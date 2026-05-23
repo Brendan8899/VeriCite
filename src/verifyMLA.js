452ec14f28eae4cd113873cee117e65529c320b7
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2026-05-23 21:07:05 +0800

    chore: clear linter issues

--- src/verifyMLA.js
+++ src/verifyMLA.js
@@ -1,4 +1,4 @@
-import { extractUrl, checkUrlExists, isValidUrl } from '../utility/utility.js';
+import { isValidUrl } from '../utility/utility.js';
 
 // MLA year is a bare 4-digit number near the end, no parentheses
 function hasYearMLA(citation) {
